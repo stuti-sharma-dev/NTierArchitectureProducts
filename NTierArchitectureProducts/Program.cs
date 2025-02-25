9d1b7680f6e731b1eca4d5bc304c9522df435a62
Student Stuti <stutisharma8912@gmail.com>
2025-02-25 16:20:02 +0530

    created the architecture and added database also

--- NTierArchitectureProducts/Program.cs
+++ NTierArchitectureProducts/Program.cs
@@ -1,6 +1,13 @@
+using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EFCoreDBConnection"));
+});
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
