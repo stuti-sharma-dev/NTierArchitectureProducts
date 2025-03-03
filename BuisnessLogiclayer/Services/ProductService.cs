1be3ec9df4f0afecbc09fc143e35336c353cad76
Student Stuti <stutisharma8912@gmail.com>
2025-03-03 17:32:55 +0530

    added changes

--- BuisnessLogiclayer/Services/ProductService.cs
+++ BuisnessLogiclayer/Services/ProductService.cs
@@ -24,30 +24,25 @@ namespace BuisnessLogiclayer.Services
             return new ProductDTO { Id = createdProduct.Id, Name = createdProduct.Name, Price = createdProduct.Price };
         }
 
-
         public async Task<bool> DeleteProduct(int id)
         {
            return await _productRepository.DeleteProduct(id);
         }
-
         public async Task<IEnumerable<ProductDTO>> GetAllProducts()
         {
             var products = await _productRepository.GetAllProducts();
             return products.Select(p => new ProductDTO { Id = p.Id, Name = p.Name, Price = p.Price , DateOfBirth=p.DateOfBirth }).ToList();
         }
-
         public async Task<ProductDTO> GetProductById(int id)
         {
             var product = await _productRepository.GetProductById(id);
             return product == null ? null : new ProductDTO { Id = product.Id, Name = product.Name, Price = product.Price , DateOfBirth=product.DateOfBirth };
         }
-
         public async Task<bool> UpdateProduct(ProductDTO productDto)
         {
             var product = new Product { Id = productDto.Id, Name = productDto.Name, Price = productDto.Price , DateOfBirth=productDto.DateOfBirth };
             return await _productRepository.UpdateProduct(product);
         }
-
 
     }
 }
